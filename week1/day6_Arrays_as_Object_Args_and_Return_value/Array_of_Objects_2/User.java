ac4418d143f3d979e24e84230de11e6e066ea55b
anirb <anirbang324@gmail.com>
2023-05-16 23:16:24 +0530

    Arrays as Object, Args and Return value - added

---
+++ week1/day6_Arrays_as_Object_Args_and_Return_value/Array_of_Objects_2/User.java
@@ -0,0 +1,35 @@
+package week1.day6_Arrays_as_Object_Args_and_Return_value.Array_of_Objects_2;
+
+public class User {
+    private String name;
+    private String mobileNumber;
+    private String username;
+    private String password;
+
+    public User(String name, String mobileNumber, String username, String password) {
+        this.name = name;
+        this.mobileNumber = mobileNumber;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String toString() {
+        return "Name:" + this.name + "\nMobile Number:" + this.mobileNumber;
+    }
+}
