8e7163203364e41c45f011359c2a020913d510d8
anirb <anirbang324@gmail.com>
2023-05-16 23:09:43 +0530

    class, object and data encapsulation added

---
+++ week1/day5_Concept_of_Classes_and_Objects/customer_address/Address.java
@@ -0,0 +1,14 @@
+package week1.day5_Concept_of_Classes_and_Objects.customer_address;
+
+public class Address{
+    String street;
+    String city;
+    int pincode;
+    String country;	
+void displayAddress() {
+System.out.println("Street: "+street);
+System.out.println("City: "+city);
+System.out.println("Pincode: "+pincode);
+System.out.println("Country: "+country);
+}
+}
