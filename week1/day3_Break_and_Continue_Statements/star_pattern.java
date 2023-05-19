5d4a45b811d95ccdf788c8241640a8fa21230aa0
anirb <anirbang324@gmail.com>
2023-05-19 12:33:40 +0530

    star_pattern

--- week1/day3_Break_and_Continue_Statements/star_pattern.java
+++ week1/day3_Break_and_Continue_Statements/star_pattern.java
@@ -3,14 +3,23 @@ package week1.day3_Break_and_Continue_Statements;
 import java.util.Scanner;
 
 public class star_pattern {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for (int i = 1; i <= n; i++) {
-          for (int j = 1; j <= i; j++) {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int i = 1;
+    while(true){
+        if(i >n)
+            break;
+        int j = 1;
+        while(true){
+            if(j > i)
+                break;
             System.out.print("*");
+            j++;
+            continue;
         }
-        System.out.println("");
-        }
-      }
+        System.out.println();
+        i++;
+    }
+}
 }
